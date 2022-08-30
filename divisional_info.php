15008b43121669157835d0b5a6af0417e582ee8a
zenBandara <chathuramahasen@gmail.com>
2022-08-30 23:39:07 +0530

    Description bug fix attempt 1 (divisional_info.php)

--- divisional_info.php
+++ divisional_info.php
@@ -27,140 +27,155 @@
 
     <title>Zonal Divisional Staff</title>
 
-<?php require_once 'nav.php'; ?>
+    <?php require_once 'nav.php'; ?>
 
-<!-- heading-->
-<style>
-    .gal:hover {
-        border: 1em #fff solid;
-        transition: .5s ease-out;
-        opacity: 0.7;
-    }
+    <!-- heading-->
+    <style>
+        .gal:hover {
+            border: 1em #fff solid;
+            transition: .5s ease-out;
+            opacity: 0.7;
+        }
 
-    .center {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-    }
-</style>
+        .center {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<section class="page-section bg-primary" id="Academic_heading" style="max-height: 20em; padding-top:6em;padding-bottom:17em;">
-    <nav aria-label="breadcrumb" style=" padding-left:2%; ">
-        <ol class="breadcrumb" style="background-color: #ffd400; width: 12em; padding-left:1em; border-radius:0.5em;">
-            <li class="breadcrumb-item"><a href="/home">Home</a></li>
-            <li class="breadcrumb-item"><a href="/zonalStaff">Staff</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Divisional Staff</li>
-        </ol>
-    </nav>
-    <div class="container px-4 px-lg-5">
-        <div class="row gx-4 gx-lg-5 justify-content-center">
-            <div class="col-lg-8 text-center">
-                <h2 class="text-white mt-0"><span style="color:#ffd400;">DIVISIONAL </span>STAFF </h2>
-                <hr class="divider divider-light" />
-                <p class="text-white-75 mb-4">The Zonal Education Office, Katugastota is facilitating supervision, education, and management over 122 government schools within 5 divisions in the Katugastota zone, Sri Lanka.</p>
-            </div>
-            
-
-        </div>
-    </div>
-</section>
-
-<!-- Services-->
-
-<br>
-<div>
-    <div class="container">
-
-        <div class="row d-flex justify-content">
-            <?php
-            $divisions = array("poojapitiya", "hatharaliyadda", "galagedara", "akurana", "harispaththuwa");
-
-            foreach ($divisions as $division) {
-
-            ?>
-
-                <div class="col-md-4" id="<?= $branch ?>">
-                    <div class="card p-3 py-4 bg-primary text-light" style="margin-bottom: 1em;">
-                        <div class="text-center">
-                            <h4 class="text-white mt-0" style="text-transform:capitalize;"><span style="color:#ffd400; text-transform:capitalize"><?= strtoupper($division); ?></span> DIVISION</h4>
-                            <hr class="divider divider-light" />
-                        </div>
-                        <?php
-                        $result_dhead = $mysqli->query("SELECT * FROM `div_staff` WHERE div_head = '$division' ") or header("location:".$query_err_page);
-                        $row_head = $result_dhead->fetch_array();
-
-
-                        ?>
-
-
-                        <div class="center"> <img src="
-                        <?php
-                                            if ($row_head['staff_img'] == "") {
-                                                echo "/default/default_pic.jpg";
-                                            } else {
-                                                echo $row_head['staff_img'];
-                                            }
-
-                                            ?>" style="width: 12rem; height:12rem; border-radius:50%"> </div>
-                        <div class="text-center mt-3">
-                            <?php
-                            if ($row_head['staff_ava'] == 'Available') : ?>
-                                <span class="bg-success p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
-                            <?php elseif ($row_head['staff_ava'] == 'Not Available') : ?>
-                                <span class="bg-danger p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
-                            <?php elseif ($row_head['staff_ava'] == 'On Study Leave') : ?>
-                                <span class="bg-warning p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
-                            <?php elseif ($row_head['staff_ava'] == 'On Maternity Leave') : ?>
-                                <span class="bg-secondary p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
-                            <?php elseif ($row_head['staff_ava'] == 'On Personal Leave') : ?>
-                                <span class="bg-default p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
-
-                            <?php endif; ?>
-                            <h5 class="mt-2 mb-0"><?php echo $row_head['staff_title'] . $row_head['staff_name']; ?></h5> <i>Divisional Director of <?php echo ucfirst($division); ?> division - Zonal Education Office, Katugastota, Sri Lanka</i>
-                            <br><i>Grade - <?php echo $row_head['staff_grade']; ?></i>
-
-                            <br>
-                            <div class="px-4 mt-1" style="color: rgba(255, 255, 255, 0.75);">
-                                <p class="fonts" style="text-align: justify;"><?php echo $row_head['staff_des']; ?></p>
-                            </div>
-                            <div>
-                                <p>
-                                    <i class="fas fa-envelope me-3"></i>
-                                    <?php echo $row_head['staff_email']; ?>
-                                </p>
-                                <p><i class="fas fa-phone me-3"></i> <?php echo $row_head['staff_contact']; ?></p>
-                            </div>
-
-                            <div> <a href="mailto:<?php echo $row_head['staff_email']; ?>" class="btn btn-light btn-xl">Message</a> </div>
-                            <hr>
-                            <div> <a href="/staffCategory/division/<?php echo $division?>" class="btn btn-light btn-xl" style="font-size:0.75em;">Check <?php echo $division?> divisional staff</a> </div>
-                        </div>
-
-
-                    </div>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <section class="page-section bg-primary" id="Academic_heading" style="max-height: 20em; padding-top:6em;padding-bottom:17em;">
+        <nav aria-label="breadcrumb" style=" padding-left:2%; ">
+            <ol class="breadcrumb" style="background-color: #ffd400; width: 12em; padding-left:1em; border-radius:0.5em;">
+                <li class="breadcrumb-item"><a href="/home">Home</a></li>
+                <li class="breadcrumb-item"><a href="/zonalStaff">Staff</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Divisional Staff</li>
+            </ol>
+        </nav>
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-lg-8 text-center">
+                    <h2 class="text-white mt-0"><span style="color:#ffd400;">DIVISIONAL </span>STAFF </h2>
+                    <hr class="divider divider-light" />
+                    <p class="text-white-75 mb-4">The Zonal Education Office, Katugastota is facilitating supervision, education, and management over 122 government schools within 5 divisions in the Katugastota zone, Sri Lanka.</p>
                 </div>
-                
+
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Services-->
+
+    <br>
+    <div>
+        <div class="container">
+
+            <div class="row d-flex justify-content">
+                <?php
+                $divisions = array("poojapitiya", "hatharaliyadda", "galagedara", "akurana", "harispaththuwa");
+
+                foreach ($divisions as $division) {
+
+                ?>
+
+                    <div class="col-md-4" id="<?= $branch ?>">
+                        <div class="card p-3 py-4 bg-primary text-light" style="margin-bottom: 1em;">
+                            <div class="text-center">
+                                <h4 class="text-white mt-0" style="text-transform:capitalize;"><span style="color:#ffd400; text-transform:capitalize"><?= strtoupper($division); ?></span> DIVISION</h4>
+                                <hr class="divider divider-light" />
+                            </div>
+                            <?php
+                            $result_dhead = $mysqli->query("SELECT * FROM `div_staff` WHERE div_head = '$division' ") or header("location:" . $query_err_page);
+                            $row_head = $result_dhead->fetch_array();
+
+
+                            ?>
+
+
+                            <div class="center"> <img src="
+                        <?php
+                        if ($row_head['staff_img'] == "") {
+                            echo "/default/default_pic.jpg";
+                        } else {
+                            echo $row_head['staff_img'];
+                        }
+
+                        ?>" style="width: 12rem; height:12rem; border-radius:50%"> </div>
+                            <div class="text-center mt-3">
+                                <?php
+                                if ($row_head['staff_ava'] == 'Available') : ?>
+                                    <span class="bg-success p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
+                                <?php elseif ($row_head['staff_ava'] == 'Not Available') : ?>
+                                    <span class="bg-danger p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
+                                <?php elseif ($row_head['staff_ava'] == 'On Study Leave') : ?>
+                                    <span class="bg-warning p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
+                                <?php elseif ($row_head['staff_ava'] == 'On Maternity Leave') : ?>
+                                    <span class="bg-secondary p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
+                                <?php elseif ($row_head['staff_ava'] == 'On Personal Leave') : ?>
+                                    <span class="bg-default p-1 px-4 rounded text-white"><?php echo $row_head['staff_ava']; ?></span>
+
+                                <?php endif; ?>
+                                <h5 class="mt-2 mb-0"><?php echo $row_head['staff_title'] . $row_head['staff_name']; ?></h5> <i>Divisional Director of <?php echo ucfirst($division); ?> division - Zonal Education Office, Katugastota, Sri Lanka</i>
+                                <br><i>Grade - <?php echo $row_head['staff_grade']; ?></i>
+
+                                <br>
+                                <!-- <div class="px-4 mt-1" style="color: rgba(255, 255, 255, 0.75);">
+                                <p class="fonts" style="text-align: justify;"></p>
+                            </div> -->
+                                <div>
+                                    <a style="color: #fff;" data-toggle="collapse" href="#col<?= $branch ?>" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                        <strong>▼ Description</strong> <i>(Click here to see)</i>
+                                    </a>
+
+                                </div>
+                                <div class="collapse" id="col<?= $branch ?>">
+
+                                    <div style="color: #661414;" class="card card-body notranslate ">
+                                        <?php echo  $row_head['staff_des']; ?>
+
+                                    </div>
+                                </div>
+                                
+                                <div>
+
+                                    <p>
+                                        <i class="fas fa-envelope me-3"></i>
+                                        <?php echo $row_head['staff_email']; ?>
+                                    </p>
+                                    <p><i class="fas fa-phone me-3"></i> <?php echo $row_head['staff_contact']; ?></p>
+                                </div>
+
+                                <div> <a href="mailto:<?php echo $row_head['staff_email']; ?>" class="btn btn-light btn-xl">Message</a> </div>
+                                <hr>
+                                <div> <a href="/staffCategory/division/<?php echo $division ?>" class="btn btn-light btn-xl" style="font-size:0.75em;">Check <?php echo $division ?> divisional staff</a> </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+
 
                 <?php } ?>
 
+            </div>
+            <div style="text-align: center;"> <a href="/divisionalDetails" class="btn btn-primary btn-xl">Divisional Statistics</a> </div>
+
+
         </div>
-        <div style="text-align: center;"> <a href="/divisionalDetails" class="btn btn-primary btn-xl">Divisional Statistics</a> </div>
-      
-    
     </div>
-</div>
 
 
-<!-- Footer-->
+    <!-- Footer-->
 
-<!-- Bootstrap core JS-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-<!-- SimpleLightbox plugin JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
-<!-- Core theme JS-->
-<script src="/js/scripts.js"></script>
-<?php require_once 'main-footer.php'; ?>
-</body>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- SimpleLightbox plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="/js/scripts.js"></script>
+    <?php require_once 'main-footer.php'; ?>
+    </body>
 
 </html>
